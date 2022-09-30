764e37f4ad6dfd54cc4bd2c45a5e5fdab54e01e9
Makaio Bolton <108162816+MakaioB@users.noreply.github.com>
2022-09-30 12:43:52 -1000

    Init commit

---
+++ AboutMe.java
@@ -0,0 +1,9 @@
+package Java_Dojo.fundamentals.First_Java_Project;
+
+public class AboutMe {
+    public static void main (String[] args) {
+        System.out.println("My name is Makaio Bolton");
+        System.out.println("I am 19 years old");
+        System.out.println("My hometown is Holualoa, HI");
+    }
+}
